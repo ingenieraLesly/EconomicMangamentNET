bbc5aa054bb072f7e37fb39f48d82e9d7151a164
ingenieraLesly <88113682+ingenieraLesly@users.noreply.github.com>
2022-04-02 19:25:36 -0500

    Delete EconomicManagementAPP directory

--- EconomicManagementAPP/Controllers/Class.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace EconomicManagementAPP.Controllers
-{
-    public class Class
-    {
-    }
-}
--- EconomicManagementAPP/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EconomicManagementAPP.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
